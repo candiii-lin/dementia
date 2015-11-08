6702def7695ef4be0e72ec0c66bbd0f034d4eea9
candice Lin <candii.lin@gmail.com>
2015-11-08 15:06:23 -0500

    edit true false

--- app/routes/alerts.js
+++ app/routes/alerts.js
@@ -2,13 +2,13 @@ var alertController = require('../controller/alerts');
 
 module.exports = function (app, passport) {
   app.post('/alerts', function (req, res) {
-     alertController.recieveAlert(!!req.body.dark, "563f57fc04e428b37dfd033a", function () {
+     alertController.recieveAlert(req.body.dark, "563f57fc04e428b37dfd033a", function () {
        res.status(200).send('OK');
      });
   });
 
   app.post('/notmoving', function (req, res) {
-    alertController.recieveNotMoving(!!req.body.dark, "563f57fc04e428b37dfd033a", function () {
+    alertController.recieveNotMoving(req.body.dark, "563f57fc04e428b37dfd033a", function () {
       res.status(200).send('OK');
     });
   });
